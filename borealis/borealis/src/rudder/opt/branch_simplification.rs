3a5e1a3dbfdad6145df45c0d97ad3318a0faf8da
Ferdia McKeogh <ferdia@mckeogh.tech>
2025-07-24 11:35:23 +0100

    Make `branch_simplication` pass detect if both blocks contain the same statements
    (we now get `(62204ms) ERROR [kernel::host::arch::x86::aarch64_mmu] invalid EL "0"` instead of the ttbr addres size fault, but it's crashing in the same place so i think we're fine)

--- borealis/borealis/src/rudder/opt/branch_simplification.rs
+++ borealis/borealis/src/rudder/opt/branch_simplification.rs
@@ -1,9 +1,14 @@
 use {
     crate::rudder::opt::OptimizationContext,
-    common::rudder::{
-        function::Function,
-        statement::{Statement, UnaryOperationKind},
+    common::{
+        arena::{Arena, Ref},
+        rudder::{
+            block::Block,
+            function::Function,
+            statement::{Statement, UnaryOperationKind},
+        },
     },
+    itertools::Itertools,
     log::trace,
 };
 
@@ -13,65 +18,97 @@ pub fn run(_ctx: &OptimizationContext, f: &mut Function) -> bool {
 
     let mut changed = false;
     for block_ref in f.block_iter().collect::<Vec<_>>() {
-        let block = block_ref.get_mut(f.arena_mut());
+        let block = block_ref.get(f.arena());
         let Some(terminator_ref) = block.terminator_statement() else {
             continue;
         };
 
-        if let Statement::Branch {
+        let Statement::Branch {
             condition,
             true_target,
             false_target,
         } = terminator_ref.get(block.arena()).clone()
-        {
-            let condition = condition.get(block.arena()).clone();
+        else {
+            continue;
+        };
 
-            if let Statement::Constant(value) = condition {
-                trace!("found constant branch statement {}", value);
+        let condition = condition.get(block.arena()).clone();
 
-                if value.is_zero() == Some(true) {
-                    terminator_ref
-                        .get_mut(block.arena_mut())
-                        .replace_kind(Statement::Jump {
-                            target: false_target,
-                        });
-                } else {
-                    terminator_ref
-                        .get_mut(block.arena_mut())
-                        .replace_kind(Statement::Jump {
-                            target: true_target,
-                        });
-                }
+        if let Statement::Constant(value) = condition {
+            trace!("found constant branch statement {}", value);
 
-                changed = true;
-            } else if let Statement::UnaryOperation { kind, value } = condition {
-                match kind {
-                    UnaryOperationKind::Not => {
-                        let new_true = false_target;
-                        let new_false = true_target;
+            let block = block_ref.get_mut(f.arena_mut());
 
-                        terminator_ref
-                            .get_mut(block.arena_mut())
-                            .replace_kind(Statement::Branch {
-                                condition: value,
-                                true_target: new_true,
-                                false_target: new_false,
-                            });
-
-                        changed = true;
-                    }
-                    _ => {}
-                }
-            } else if true_target == false_target {
+            if value.is_zero() == Some(true) {
+                terminator_ref
+                    .get_mut(block.arena_mut())
+                    .replace_kind(Statement::Jump {
+                        target: false_target,
+                    });
+            } else {
                 terminator_ref
                     .get_mut(block.arena_mut())
                     .replace_kind(Statement::Jump {
                         target: true_target,
                     });
-                changed = true;
             }
+
+            changed = true;
+        } else if let Statement::UnaryOperation { kind, value } = condition {
+            match kind {
+                UnaryOperationKind::Not => {
+                    let new_true = false_target;
+                    let new_false = true_target;
+
+                    let block = block_ref.get_mut(f.arena_mut());
+
+                    terminator_ref
+                        .get_mut(block.arena_mut())
+                        .replace_kind(Statement::Branch {
+                            condition: value,
+                            true_target: new_true,
+                            false_target: new_false,
+                        });
+
+                    changed = true;
+                }
+                _ => {}
+            }
+        } else if true_target == false_target
+            || equivalent_blocks(f.arena(), true_target, false_target)
+        {
+            let block = block_ref.get_mut(f.arena_mut());
+
+            terminator_ref
+                .get_mut(block.arena_mut())
+                .replace_kind(Statement::Jump {
+                    target: true_target,
+                }); // todo: verify this will be inlined/threaded if needed?
+            changed = true;
         }
     }
 
     changed
+}
+
+/// Returns whether two blocks contain the same statements
+///
+/// Todo: make this smarter and detect
+fn equivalent_blocks(arena: &Arena<Block>, a: Ref<Block>, b: Ref<Block>) -> bool {
+    let block_a = a.get(arena);
+    let block_b = b.get(arena);
+
+    !block_a
+        .statements()
+        .iter()
+        // panic if not equal length
+        .zip_eq(block_b.statements().iter())
+        // if any statements are not equal, short circuits returning true, which we then `not` at
+        // the beginning of the iterator
+        .any(|(ref_a, ref_b)| {
+            let statement_a = ref_a.get(block_a.arena());
+            let statement_b = ref_b.get(block_b.arena());
+
+            statement_a.to_string(block_a.arena()) != statement_b.to_string(block_b.arena())
+        })
 }
