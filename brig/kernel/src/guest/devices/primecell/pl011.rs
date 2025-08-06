9a69ded122e3f8580aae0e39117d6d09a045a571
Ferdia McKeogh <ferdia@mckeogh.tech>
2025-08-06 16:00:28 +0100

    Implement more PL011 functionality (still no output from hello init)

--- brig/kernel/src/guest/devices/primecell/pl011.rs
+++ brig/kernel/src/guest/devices/primecell/pl011.rs
@@ -6,12 +6,14 @@ use {
     alloc::{collections::BTreeMap, sync::Arc},
     common::intern::InternedString,
     proc_macro_lib::guest_device_factory,
+    spin::Mutex,
 };
 
 #[guest_device_factory(pl011)]
 fn create_pl011(_config: &BTreeMap<InternedString, InternedString>) -> Arc<dyn Device> {
     let dev = Arc::new(Pl011 {
         id: ObjectId::new(),
+        registers: Mutex::new(Registers::new()),
     });
 
     dev
@@ -20,6 +22,7 @@ fn create_pl011(_config: &BTreeMap<InternedString, InternedString>) -> Arc<dyn D
 #[derive(Debug)]
 struct Pl011 {
     id: ObjectId,
+    registers: Mutex<Registers>,
 }
 
 impl Object for Pl011 {
@@ -42,19 +45,56 @@ impl MemoryMappedDevice for Pl011 {
         0x1000
     }
 
-    /// Read `value.len()` bytes from the device starting at `offset`
-    fn read(&self, _offset: u64, value: &mut [u8]) {
-        // return all zeros for now
-        value.fill(0);
+    fn read(&self, offset: u64, value: &mut [u8]) {
+        self.registers.lock().read(offset, value);
     }
 
-    /// Write `value` bytes into the device starting at `offset`
     fn write(&self, offset: u64, value: &[u8]) {
-        match (offset, value) {
-            (0x0000, _) => crate::print!("{}", value[0] as char),
+        self.registers.lock().write(offset, value);
+    }
+}
 
-            // todo: https://developer.arm.com/documentation/ddi0183/g/programmers-model/summary-of-registers
-            (offset, value) => log::debug!("PL011: wrote {value:x?} @ {offset:x}"),
+#[derive(Debug)]
+struct Registers {}
+
+impl Registers {
+    fn new() -> Self {
+        Self {}
+    }
+
+    fn write(&mut self, offset: u64, src: &[u8]) {
+        match offset {
+            0x000 => crate::print!("{}", src[0] as char),
+            0x004 => (),
+            0x018 => panic!("read only register"),
+            0x008..0x014 | 0x01C | 0x04C..0x07C | 0x090..0xFCC => {
+                panic!("reserved")
+            }
+            0x080..0x08C => panic!("reserved for test purposes"),
+            0xFD0..0xFDC => panic!("reserved for future ID expansion"),
+            offset => unreachable!("got write on offset {offset:x}"),
+        }
+    }
+
+    fn read(&mut self, offset: u64, dst: &mut [u8]) {
+        match offset {
+            0x000 => (), // todo: read from stdin or something
+            0x004 => dst.fill(0),
+            0x018 => dst[0] = 0b010010000,
+            0x008..0x014 | 0x01C | 0x04C..0x07C | 0x090..0xFCC => {
+                panic!("reserved")
+            }
+            0x080..0x08C => panic!("reserved for test purposes"),
+            0xFD0..0xFDC => panic!("reserved for future ID expansion"),
+            0xFE0 => dst[0] = 0x11,
+            0xFE4 => dst[0] = 0x10,
+            0xFE8 => dst[0] = 0x04,
+            0xFEC => dst[0] = 0x00,
+            0xFF0 => dst[0] = 0x0D,
+            0xFF4 => dst[0] = 0xF0,
+            0xFF8 => dst[0] = 0x05,
+            0xFFC => dst[0] = 0xB1,
+            offset => unreachable!("got read on offset {offset:x}"),
         }
     }
 }
